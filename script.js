d00c481db243acf297934a2b2b9dc2eb457c348c
jakobsisk <a14jaksi@student.his.se>
2017-03-16 14:05:57 +0100

    Mouse movement geometric progression check 1.0

--- script.js
+++ script.js
@@ -8,7 +8,12 @@
 // Descides how arithmetic progression is defined on mouse movement
 const ARIT_LIMIT = 2;
 // Defines how much arithmetic progression effects the user rating
-const ARIT_STRNGTH = 1;
+const ARIT_STRNGTH = 2;
+
+// Descides how geometric progression is defined on mouse movement
+const GEO_LIMIT = 0.0010;
+// Defines how much geometric progression effects the user rating
+const GEO_STRNGTH = 5;
 
 var userRating;
 
@@ -95,141 +100,36 @@ function refreshPage()
 
 
     // Measure time spent on each text input
+    textInputs.each(inputTimer);
 
-    textInputs.each(function(i, e) 
+    // Mouse movement analysis
+
+    var i = 0;
+    var arrX = new Array();
+    var arrY = new Array();
+
+    $(document).on('mousemove', function(event)
     {
-      var timeInputStart;
-      var timeInputEnd;
-      var timeInput;
+      // Handle every 10th mouse event (minimize performance impact)
 
-      $(e).on('focus', function() 
-      {
-        timeInputStart = new Date();
-      });
-      $(e).on('blur', function() 
-      {
-        timeInputEnd = new Date();
-        timeInput = timeInputEnd - timeInputStart;
+      if ((i + 1) % 10 === 0) {
+        arrX[(i + 1) / 10] = (-event.pageX > 0) ? -event.pageX : event.pageX;
+        arrY[(i + 1) / 10] = (-event.pageY > 0) ? -event.pageY : event.pageY;
 
-        console.log('DATA:');
-        console.log('  Input "' + $(e).attr('name') + '" time - ' + timeInput + 'ms');
+        // Analayze if mouse movements follow arithmetic progression
+        aritProgrCheck(i, arrX, arrY);
+        geoProgrCheck(i, arrX, arrY);
+      }
 
-        var ratingChange = 0;
-        if ($(e).val() != 'undefined' && $(e).val() != '') {
-          // If user finishes input to quickly
-          if (timeInput < 1000) {
-            ratingChange = -5;
-          }
-          // User finished input slowly
-          else {
-            ratingChange = 5;
-          }
-        }
-        // If input is finished empty
-        else if (timeInput > 1000) {
-          ratingChange = 10;
-        }
-        modRating(ratingChange);
-      });
+      if (i >= 99) {
+        diff = new Array();
+        i = 0;
+      }
+      else {
+        i++;
+      }
     });
   }
-
-
-  // Mouse movement analysis
-
-  var i = 0;
-  var arrX = new Array();
-  var arrY = new Array();
-
-  $(document).on('mousemove', function(event)
-  {
-    // Handle every 10th mouse event (minimize performance impact)
-
-    if ((i + 1) % 10 === 0) {
-      arrX[(i + 1) / 10] = (-event.pageX > 0) ? -event.pageX : event.pageX;
-      arrY[(i + 1) / 10] = (-event.pageY > 0) ? -event.pageY : event.pageY;
-
-      var diffs = new Array();
-      var diffsDiffs = new Array();
-
-      if (i > 0 && ((i + 1) % 100) === 0) {
-
-        // Get mean difference of mouse movement progression 
-        //    or difference of difference of mouse coordinates
-        // Low difference = arithmetic progression = bot-like behaviour
-
-        var subtr;
-        var diff;
-        var meanX;
-        var meanY;
-        var mean;
-
-        // X mean difference
-
-        for (j = 1; j < (i + 1) / 10; j++) {
-          subtr = arrX[j + 1] - arrX[j];
-          diff = (-subtr > 0) ? -subtr : subtr;
-          diffs.push(diff);
-        }
-
-        for (j = 0; j < diffs.length - 1; j++) {
-          subtr = diffs[j + 1] - diffs[j];
-          diffDiff = (-subtr > 0) ? -subtr : subtr;
-          diffsDiffs.push(diffDiff);
-        }
-
-        var sum = 0;
-
-        for (j = 0; j < diffsDiffs.length; j++) {
-          sum += diffsDiffs[j];
-        }
-
-        meanX = sum / diffsDiffs.length;
-        meanX = +(Math.round(meanX + "e+2")  + "e-2");
-        meanX = (-meanX > 0) ? -meanX: meanX;
-
-        // Y mean difference
-
-        for (j = 1; j < (i + 1) / 10; j++) {
-          subtr = arrY[j + 1] - arrY[j];
-          diff = (-subtr > 0) ? -subtr : subtr;
-          diffs.push(diff);
-        }
-
-        for (j = 0; j < diffs.length - 1; j++) {
-          subtr = diffs[j + 1] - diffs[j];
-          diffDiff = (-subtr > 0) ? -subtr : subtr;
-          diffsDiffs.push(diffDiff);
-        }
-
-        var sum = 0;
-        
-        for (j = 0; j < diffsDiffs.length; j++) {
-          sum += diffsDiffs[j];
-        }
-
-        meanY = sum / diffsDiffs.length;
-        meanY = +(Math.round(meanY + "e+2")  + "e-2");
-        meanY = (-meanY > 0) ? -meanY: meanY;
-
-        mean = (meanX + meanY) / 2;
-
-        console.log('DATA:');
-        console.log('  Mouse movement - ' + mean);
-
-        ratingChange = (mean < ARIT_LIMIT) ? -ARIT_STRNGTH : ARIT_STRNGTH;
-        modRating(ratingChange);
-      }
-    }
-
-    if (i >= 99) {
-      diff = new Array();
-      i = 0;
-    }
-    else {
-      i++;
-    }
-  });
 
   // --- /New CAPTCHA --- //
 
@@ -249,6 +149,183 @@ function toggleNav()
   else {
     // Hide nav
     nav.css('height', 0);
+  }
+}
+
+function inputTimer(i, e)
+{
+  var timeInputStart;
+  var timeInputEnd;
+  var timeInput;
+
+  $(e).on('focus', function() 
+  {
+    timeInputStart = new Date();
+  });
+  $(e).on('blur', function() 
+  {
+    timeInputEnd = new Date();
+    timeInput = timeInputEnd - timeInputStart;
+
+    console.log('DATA:');
+    console.log('  Input "' + $(e).attr('name') + '" time - ' + timeInput + 'ms');
+
+    var ratingChange = 0;
+    if ($(e).val() != 'undefined' && $(e).val() != '') {
+      // If user finishes input to quickly
+      if (timeInput < 1000) {
+        ratingChange = -5;
+      }
+      // User finished input slowly
+      else {
+        ratingChange = 5;
+      }
+    }
+    // If input is finished empty
+    else if (timeInput > 1000) {
+      ratingChange = 10;
+    }
+    modRating(ratingChange);
+  });
+}
+
+function aritProgrCheck(i, arrX, arrY) 
+{
+  var diffs = new Array();
+  var diffsDiffs = new Array();
+
+  if (i > 0 && ((i + 1) % 100) === 0) {
+
+    // Get mean difference of mouse movement progression 
+    //    or difference of difference of mouse coordinates
+    // Low difference = arithmetic progression = bot-like behaviour
+
+    var subtr;
+    var diff;
+    var diffsDiff;
+    var meanX;
+    var meanY;
+    var mean;
+    var sum = 0;
+
+    // X mean difference
+
+    for (j = 1; j < (i + 1) / 10; j++) {
+      subtr = arrX[j + 1] - arrX[j];
+      diff = (-subtr > 0) ? -subtr : subtr;
+      diffs.push(diff);
+    }
+
+    for (j = 0; j < diffs.length - 1; j++) {
+      subtr = diffs[j + 1] - diffs[j];
+      diffsDiff = (-subtr > 0) ? -subtr : subtr;
+      diffsDiffs.push(diffsDiff);
+    }
+
+    for (j = 0; j < diffsDiffs.length; j++) {
+      sum += diffsDiffs[j];
+    }
+
+    meanX = sum / diffsDiffs.length;
+    meanX = (-meanX > 0) ? -meanX: meanX;
+
+    // Y mean difference
+
+    for (j = 1; j < (i + 1) / 10; j++) {
+      subtr = arrY[j + 1] - arrY[j];
+      diff = (-subtr > 0) ? -subtr : subtr;
+      diffs.push(diff);
+    }
+
+    for (j = 0; j < diffs.length - 1; j++) {
+      subtr = diffs[j + 1] - diffs[j];
+      diffsDiff = (-subtr > 0) ? -subtr : subtr;
+      diffsDiffs.push(diffsDiff);
+    }
+
+    sum = 0;
+    
+    for (j = 0; j < diffsDiffs.length; j++) {
+      sum += diffsDiffs[j];
+    }
+
+    meanY = sum / diffsDiffs.length;
+    meanY = (-meanY > 0) ? -meanY: meanY;
+
+    mean = (meanX + meanY) / 2;
+    mean = Math.round(mean * 100) / 100; // Limit to 2 decimals
+
+    console.log('DATA:');
+    console.log('  Mouse movement difference - ' + mean);
+
+    ratingChange = (mean < ARIT_LIMIT) ? -ARIT_STRNGTH : 1;
+    modRating(ratingChange);
+  }
+}
+
+function geoProgrCheck(i, arrX, arrY) 
+{
+  var quots = new Array();
+  var quotsDiffs = new Array();
+
+  if (i > 0 && ((i + 1) % 100) === 0) {
+
+    // Get mean quotient of mouse movement progression 
+    //    or difference of quotient of mouse coordinates
+    // Low quotient = geometric progression = bot-like behaviour
+
+    var quot;
+    var quotsDiff;
+    var meanX;
+    var meanY;
+    var mean;
+    var sum = 0;
+
+    // X mean quotient
+
+    for (j = 1; j < (i + 1) / 10; j++) {
+      quot = arrX[j + 1] / arrX[j];
+      quots.push(quot);
+    }
+
+    for (j = 0; j < quots.length - 1; j++) {
+      quotsDiff = quots[j + 1] - quots[j];
+      quotsDiffs.push(quotsDiff);
+    }
+
+    for (j = 0; j < quotsDiffs.length; j++) {
+      sum += quotsDiffs[j];
+    }
+
+    meanX = sum / quotsDiffs.length;
+
+    // Y mean quotient
+
+    for (j = 1; j < (i + 1) / 10; j++) {
+      quot = arrY[j + 1] / arrY[j];
+      quots.push(quot);
+    }
+
+    for (j = 0; j < quots.length - 1; j++) {
+      quotsDiff = quots[j + 1] - quots[j];
+      quotsDiffs.push(quotsDiff);
+    }
+
+    for (j = 0; j < quotsDiffs.length; j++) {
+      sum += quotsDiffs[j];
+    }
+
+    meanY = sum / quotsDiffs.length;
+
+    mean = (meanX + meanY) / 2;
+    mean = Math.round(mean * 10000) / 10000; // Limit to 2 decimals
+    mean = (-mean > 0) ? -mean : mean;
+
+    console.log('DATA:');
+    console.log('  Mouse movement quotient - ' + mean);
+
+    ratingChange = (mean < GEO_LIMIT) ? -GEO_STRNGTH : 1;
+    modRating(ratingChange);
   }
 }
 
